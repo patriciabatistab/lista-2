7a929fb1d2efb90340b4ee9245a674a99fa8a2aa
richh-brreto <richard.barreto@sptech.school>
2026-04-13 11:26:39 -0300

    exercicio 6

---
+++ src/main/java/org/example/richard/Exercicio6.java
@@ -0,0 +1,10 @@
+package org.example.richard;
+
+public class Exercicio6 {
+    public static void main(String[] args) {
+        int raio = 5;
+        double area = Math.PI * Math.pow(raio, 2);
+
+        System.out.println("Area do círculo: " + area);
+    }
+}
