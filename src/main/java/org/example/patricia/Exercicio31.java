30777834adb54e575e345438a2a71bc5afc26155
patriciabatsistab <patricia02borges@gmail.com>
2026-04-13 14:44:55 -0300

    exercicio 31

---
+++ src/main/java/org/example/patricia/Exercicio31.java
@@ -0,0 +1,9 @@
+package org.example.patricia;
+
+public class Exercicio31 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(100);
+        }
+    }
+}
