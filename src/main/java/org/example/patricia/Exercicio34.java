3a76fb0759e1730c0acb87264e74d848a3860ef8
patriciabatsistab <patricia02borges@gmail.com>
2026-04-13 14:52:15 -0300

    exercicio 34

---
+++ src/main/java/org/example/patricia/Exercicio34.java
@@ -0,0 +1,10 @@
+package org.example.patricia;
+
+public class Exercicio34 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            int numeroSorteado = (int) ((Math.random() * 100) + 1);
+            System.out.println(numeroSorteado);
+        }
+    }
+}
