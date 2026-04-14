6c7ac8de33810387d758b4386d354466d187d5b9
richh-brreto <richard.barreto@sptech.school>
2026-04-14 14:39:32 -0300

    exercicio 42

--- src/main/java/org/example/richard/Exercicio42.java
+++ src/main/java/org/example/richard/Exercicio42.java
@@ -1,7 +1,20 @@
 package org.example.richard;
 
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercicio42 {
     public static void main(String[] args) {
-        // precisa do 41
+        System.out.println("40 grupos para sorteio:");
+
+        for (int i = 1; i <= 40; i++) {
+            int[] grupo = new int[3];
+            for (int j = 0; j < 3; j++) {
+                grupo[j] = ThreadLocalRandom.current().nextInt(0, 60);
+            }
+            Arrays.sort(grupo);
+
+            System.out.printf("Grupo %d: %d, %d, %d\n", i, grupo[0], grupo[1], grupo[2]);
+        }
     }
 }
