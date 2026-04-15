304b17ede19dc9ac5142a581bc1ff8ec5b2e447e
patriciabatsistab <patricia02borges@gmail.com>
2026-04-15 14:46:25 -0300

    Exercicio 64

--- src/main/java/org/example/patricia/Exercicio64.java
+++ src/main/java/org/example/patricia/Exercicio64.java
@@ -1,4 +1,20 @@
 package org.example.patricia;
 
+import java.util.Scanner;
+
 public class Exercicio64 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de triângulos: ");
+        int qtdTriangulo = scanner.nextInt();
+
+        String asteristico = "*";
+        for (int i = 1; i <= qtdTriangulo; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.println(asteristico.repeat(j));
+            }
+            System.out.println("");
+        }
+    }
 }
